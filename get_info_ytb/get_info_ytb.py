8a799530188923f91007eeb8c2f126a73adce54e
BluePhoenix <69658131+BluePhoenixxx@users.noreply.github.com>
2022-11-03 20:44:29 +0700

    Update get_info_ytb.py

--- get_info_ytb/get_info_ytb.py
+++ get_info_ytb/get_info_ytb.py
@@ -1,18 +1,29 @@
+# Create a class video , class hava title and link
 class Video:
 	def __init__(self, title, link):
 		self.title = title
 		self.link = link
-
+#Create a class playlist , class have name, description , rating ,and class videos
+class Playlist:
+	def __init__(self, name, description, rating, videos):
+		self.name = name
+		self.description = description
+		self.rating = rating
+		self.videos = videos
+#Ask an user enter title and link
 def read_video():
 	title = input("Enter title: ")
 	link = input("Enter link: ")
 	video = Video(title, link)
 	return video
 
+#print title and link of video
 def print_video(video):
-	print("Video title: ", video.title)
-	print("Video link: ", video.link)
+	print("Video title: ", video.title, end="")
+	print("Video link: ", video.link, end="")
 
+
+#Ask an user ttiel and link cog am viedo
 def read_videos():
 	videos = []
 	total_video = int(input("Enter how many videos: "))
@@ -22,26 +33,62 @@ def read_videos():
 		videos.append(vid)
 	return videos
 
+
+#print vide3os
 def print_videos(videos):
+	print("---")
 	for i in range(len(videos)):
 		print_video(videos[i])
 
+
+#write class videos for  in tdxt
 def write_video_txt(video, file):
 	file.write(video.title + "\n")
 	file.write(video.link + "\n")
 
-def write_to_txt(videos):
+
+#
+def write_videos_txt(videos, file):
 	total = len(videos)
+	file.write(str(total) + "\n")
+	for i in range(total):
+		write_video_txt(videos[i], file)
+
+def read_video_from_txt(file):
+	title = file.readline()
+	link = file.readline()
+	video = Video(title, link)
+	return video
+
+def read_videos_from_txt():
+	videos = []
+	with open("data.txt", "r") as file:
+		total = file.readline()		
+		for i in range(int(total)):
+			video = read_video_from_txt(file)
+			videos.append(video)
+	return videos
+
+def read_playlist():
+	playlist_name = input("Enter playlist name: ")
+	playlist_description = input("Enter playlist description: ")
+	playlist_rating = input("Enter rating (1-5): ")
+	playlist_videos = read_videos()
+	playlist = Playlist(playlist_name, playlist_description, playlist_rating, playlist_videos)
+	return playlist
+
+def write_playlist_txt(playlist):
 	with open("data.txt", "w") as file:
-		file.write(str(total) + "\n")
-		for i in range(total):
-			write_video_txt(videos[i], file)
+		file.write(playlist.name + "\n")
+		file.write(playlist.description + "\n")
+		file.write(playlist.rating + "\n")
+		write_videos_txt(playlist.videos, file)
+	print("Successfully write playlist to txt")
 
 def main():
-	videos = read_videos()
-	write_to_txt(videos)	
-	print("---")
-	print_videos(videos)
-
+	playlist = read_playlist()
+	write_playlist_txt(playlist)
+	# playlist = read_playlist_from_txt()
+	# print_playlist(playlist)
 
 main()

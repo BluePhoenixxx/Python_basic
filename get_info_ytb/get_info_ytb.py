c3a30827b3a6f6beaf1247ecff1a19179aefcca2
bian197 <biendien4508@gmail.com>
2022-11-03 21:04:31 +0700

    Hoan thanh

--- get_info_ytb/get_info_ytb.py
+++ get_info_ytb/get_info_ytb.py
@@ -1,42 +1,66 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 # Create a class video , class hava title and link
 >>>>>>> 6da28ad (add class playlist)
+=======
+import webbrowser
+
+>>>>>>> 475a4b8 (Hoan thanh)
 class Video:
 	def __init__(self, title, link):
 		self.title = title
 		self.link = link
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 #Create a class playlist , class have name, description , rating ,and class videos
+=======
+		self.seen = False
+
+	def open(self):
+		webbrowser.open(self.link)
+		self.seen = True
+
+>>>>>>> 475a4b8 (Hoan thanh)
 class Playlist:
 	def __init__(self, name, description, rating, videos):
 		self.name = name
 		self.description = description
 		self.rating = rating
 		self.videos = videos
+<<<<<<< HEAD
 #Ask an user enter title and link
 >>>>>>> 6da28ad (add class playlist)
+=======
+
+>>>>>>> 475a4b8 (Hoan thanh)
 def read_video():
-	title = input("Enter title: ")
-	link = input("Enter link: ")
+	title = input("Enter title: ") + "\n"
+	link = input("Enter link: ") + "\n"
 	video = Video(title, link)
 	return video
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 #print title and link of video
 >>>>>>> 6da28ad (add class playlist)
+=======
+>>>>>>> 475a4b8 (Hoan thanh)
 def print_video(video):
 	print("Video title: ", video.title, end="")
 	print("Video link: ", video.link, end="")
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 #Ask an user ttiel and link cog am viedo
 >>>>>>> 6da28ad (add class playlist)
+=======
+>>>>>>> 475a4b8 (Hoan thanh)
 def read_videos():
 	videos = []
 	total_video = int(input("Enter how many videos: "))
@@ -47,29 +71,38 @@ def read_videos():
 	return videos
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 #print vide3os
 >>>>>>> 6da28ad (add class playlist)
+=======
+>>>>>>> 475a4b8 (Hoan thanh)
 def print_videos(videos):
-	print("---")
 	for i in range(len(videos)):
+		print("Video " + str(i+1) + ":")
 		print_video(videos[i])
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 
 #write class videos for  in tdxt
 >>>>>>> 6da28ad (add class playlist)
+=======
+>>>>>>> 475a4b8 (Hoan thanh)
 def write_video_txt(video, file):
-	file.write(video.title + "\n")
-	file.write(video.link + "\n")
+	file.write(video.title)
+	file.write(video.link)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 def write_to_txt(videos):
 =======
 
 #
+=======
+>>>>>>> 475a4b8 (Hoan thanh)
 def write_videos_txt(videos, file):
 >>>>>>> 6da28ad (add class playlist)
 	total = len(videos)
@@ -83,39 +116,165 @@ def read_video_from_txt(file):
 	video = Video(title, link)
 	return video
 
-def read_videos_from_txt():
+def read_videos_from_txt(file):
 	videos = []
-	with open("data.txt", "r") as file:
-		total = file.readline()		
-		for i in range(int(total)):
-			video = read_video_from_txt(file)
-			videos.append(video)
+	total = file.readline()		
+	for i in range(int(total)):
+		video = read_video_from_txt(file)
+		videos.append(video)
 	return videos
 
 def read_playlist():
-	playlist_name = input("Enter playlist name: ")
-	playlist_description = input("Enter playlist description: ")
-	playlist_rating = input("Enter rating (1-5): ")
+	playlist_name = input("Enter playlist name: ") + "\n"
+	playlist_description = input("Enter playlist description: ") + "\n"
+	playlist_rating = input("Enter rating (1-5): ") + "\n"
 	playlist_videos = read_videos()
 	playlist = Playlist(playlist_name, playlist_description, playlist_rating, playlist_videos)
 	return playlist
 
 def write_playlist_txt(playlist):
 	with open("data.txt", "w") as file:
-		file.write(playlist.name + "\n")
-		file.write(playlist.description + "\n")
-		file.write(playlist.rating + "\n")
+		file.write(playlist.name)
+		file.write(playlist.description)
+		file.write(playlist.rating)
 		write_videos_txt(playlist.videos, file)
 	print("Successfully write playlist to txt")
 
-def main():
-	playlist = read_playlist()
-	write_playlist_txt(playlist)
-	# playlist = read_playlist_from_txt()
-	# print_playlist(playlist)
+def read_playlist_from_txt():
+	with open("data.txt", "r") as file:
+		playlist_name = file.readline()
+		playlist_description = file.readline()
+		playlist_rating = file.readline()
+		playlist_videos = read_videos_from_txt(file)
+	playlist = Playlist(playlist_name, playlist_description, playlist_rating, playlist_videos)
+	return playlist
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 main()
 =======
 main()
 >>>>>>> 6da28ad (add class playlist)
+=======
+def print_playlist(playlist):
+	print("-------")
+	print("Playlist name: " +  playlist.name, end="")
+	print("Playlist description: " +  playlist.description, end="")
+	print("Playlist rating: " +  playlist.rating, end="")
+	print_videos(playlist.videos)
+
+def show_menu():
+	print("Main Menu:")
+	print("-----------------------------")
+	print("| Option 1: Create playlist |")
+	print("| Option 2: Show playlist   |")
+	print("| Option 3: Play a video    |")
+	print("| Option 4: Add a video     |")
+	print("| Option 5: Update playlist |")
+	print("| Option 6: Remove video    |")
+	print("| Option 7: Save and Exit   |")
+	print("-----------------------------")
+
+def select_in_range(prompt, min, max):
+	choice = input(prompt)
+	while not choice.isdigit() or int(choice) < min or int(choice) > max:
+		choice = input(prompt)
+
+	choice = int(choice)
+	return choice
+
+def play_video(playlist):
+	print_videos(playlist.videos)
+	total = len(playlist.videos)
+
+	choice = select_in_range("Select a video (1," + str(total) + "): " , 1,total)
+	print("Open video: " + playlist.videos[choice-1].title + " - " + playlist.videos[choice-1].link, end ="")
+	playlist.videos[choice-1].open()
+
+def add_video(playlist):
+	print("Enter new video information:")
+	new_video_title = input("Enter new video title: ") + "\n"
+	new_video_link =  input("Enter new video link: ") + "\n"
+	new_video = Video(new_video_title, new_video_link)
+	playlist.videos.append(new_video)
+	return playlist
+
+def update_playlist(playlist):
+	# Update name / description / rating
+	print("Update playlist?")
+	print("1. Name")	
+	print("2. Description")	
+	print("3. Rating")	
+
+	choice = select_in_range("Enter what you want to update (1-3):", 1,3)
+	if choice == 1:
+		new_playlist_name = input("Enter new name for playlist: ") + "\n"
+		playlist.name = new_playlist_name
+		print("Updated Successfully !")
+		return playlist
+	if choice == 2:
+		new_playlist_description = input("Enter new description for playlist: ") + "\n"
+		playlist.description = new_playlist_description
+		print("Updated Successfully !")
+		return playlist
+	if choice == 3:
+		new_playlist_rating = str(select_in_range("Enter new rating (1-5): ",1,5)) + "\n"
+		playlist.rating = new_playlist_rating
+		print("Updated Successfully !")
+		return playlist
+
+def remove_video(playlist):
+	print_videos(playlist.videos)
+	choice = select_in_range("Enter video you want to delete: ",1,len(playlist.videos))
+	new_video_list = []
+	# del playlist.videos[choice-1]
+	for i in range(len(playlist.videos)):
+		if i == choice-1:
+			continue
+		new_video_list.append(playlist.videos[i])
+
+	playlist.videos = new_video_list
+
+	print("Delete Successfully !!!")
+	return playlist
+
+
+
+def main():
+	try:
+		playlist = read_playlist_from_txt()
+		print("Loaded data Successfully !!!")
+	except:
+		print("Welcome first user !!!")		
+
+	while True:
+		show_menu()
+		choice = select_in_range("Select an option (1-7):", 1, 7)
+		if choice == 1:
+			playlist = read_playlist()	
+			input("\nPress Enter to continue.\n")	
+		elif choice == 2:
+			print_playlist(playlist)
+			input("\nPress Enter to continue.\n")	
+		elif choice == 3:
+			play_video(playlist)	
+			input("\nPress Enter to continue.\n")	
+		elif choice == 4:
+			playlist = add_video(playlist)	
+			input("\nPress Enter to continue.\n")
+		elif choice == 5:
+			playlist = update_playlist(playlist)	
+			input("\nPress Enter to continue.\n")
+		elif choice == 6:
+			playlist = remove_video(playlist)	
+			input("\nPress Enter to continue.\n")
+		elif choice == 7:
+			write_playlist_txt(playlist)
+			input("\nPress Enter to continue.\n")	
+			break
+		else:
+			print("Wrong Input, Exist.")
+			break
+			
+main()
+>>>>>>> 475a4b8 (Hoan thanh)
